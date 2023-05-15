1b87ccee33c14b184de29945e0d740a2fccf601d
Aftermath001 <alvinasiachi@gmail.com>
2023-05-15 20:02:58 +0300

    Done

---
+++ lib/app.py
@@ -0,0 +1,25 @@
+# def pythonNum():
+#    listNum =[5,6,8,15,2]
+#    listNum.sort()
+#    print(listNum[0:5:4])
+
+# def pythonNum(listNum):
+#     listNum =[5,6,8,15,2]
+#     # for i in range(listNum):
+#     print(max(listNum))
+# list= [5,6,8,15,2]
+# list.sort()
+# print("min=",min(list))
+# print("max=",max(list))
+# print(list[-2])
+
+l = [5,6,8,15,2,7]
+
+def number(l):    
+    l.sort()
+    print("min=",min(l))
+    print("max=",max(l))
+    print(l[-2])
+
+number(l)
+# print(len(l))
